e155756bf79dd6e29050259140a41eb3895041ba
Chris Karampagias <karampagiaschris@gmail.com>
2022-09-03 10:19:11 +0300

    Rework entire script
    Added more functions for clarity, an option to restart the game and an
    image displaying the computer's choice each round.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let i = 0;
+let playerWins;
+let computerWins;
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const result = document.querySelector("#result");
+const buttons = document.querySelectorAll(".grow");
+const choiceContainer = document.querySelector(".computerChoice");
+const resultContainer = document.querySelector(".resultContainer");
+const roundResult = document.querySelector("#roundResult");
+buttons.forEach(button => button.addEventListener("click",playGame));
+
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch (num){
@@ -11,9 +24,9 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice(button) {
-    if (button.target.getAttribute("id") == 'rock'){
+    if (button.target.classList.contains("rock") == true){
         playerSelection = "rock";
-    }else if (button.target.getAttribute("id") == "paper"){
+    }else if (button.target.classList.contains("paper") == true){
         playerSelection = "paper";
     }else{
         playerSelection = "scissors";
@@ -23,82 +36,114 @@ function getPlayerChoice(button) {
 
 function playRound(playerSelection,computerSelection){  
     if (playerSelection == computerSelection ){
-        message.textContent = "It's a tie!";
+        roundResult.textContent = "Tie!";
         return "Tie";
     }else if (playerSelection == "rock" && computerSelection == "paper"){
-        message.textContent = "You Lose! Paper beats Rock";
+        roundResult.textContent = "You Lose!";
         return "Defeat";
     }else if (playerSelection == "rock" && computerSelection == "scissors"){
-        message.textContent = "You Win! Rock beats Scissors";
+        roundResult.textContent = "You Win!";
         return "Victory";
     }else if (playerSelection == "paper" && computerSelection == "rock"){
-        message.textContent = "You Win! Paper beats Rock";
+        roundResult.textContent = "You Win!";
         return "Victory";
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
-        message.textContent = "You Lose! Scissors beat Paper";
+        roundResult.textContent = "You Lose!";
         return "Defeat";
     }else if (playerSelection == "scissors" && computerSelection == "paper"){
-        message.textContent = "You Win! Scissors beat Paper";
+        roundResult.textContent = "You Win!";
         return "Victory";
     }else if (playerSelection == "scissors" && computerSelection == "rock"){
-        message.textContent = "You Lose! Rock beats Scissors";
+        roundResult.textContent = "You Lose!";
         return "Defeat";
     }
 }   
 
-    
+function addPoints(outcome){
+    switch (outcome) {
+        case "Tie":
+            playerWins++;
+            computerWins++;
+            playerScore.textContent = `Player Score : ${playerWins}`;
+            computerScore.textContent = `Computer Score : ${computerWins}`;
+            break;
+        case "Victory":
+            playerWins++;
+            playerScore.textContent = `Player Score : ${playerWins}`;
+            computerScore.textContent = `Computer Score : ${computerWins}`;
+            break;
+        case "Defeat":
+            computerWins++;
+            playerScore.textContent = `Player Score : ${playerWins}`;
+            computerScore.textContent = `Computer Score : ${computerWins}`;
+            break;
+    }
+}
 function initializeGame(){
-    let i = 1;
-    let playerWins = 0;
-    let computerWins = 0;
-    const btns = document.querySelectorAll('button');
-    const message = document.querySelector("#message");
-    const playerScore = document.querySelector("#player-score")
-    const computerScore = document.querySelector("#computer-score");
-    const result = document.querySelector("#result");
+    i = 1;
+    playerWins = 0;
+    computerWins = 0;
+}
 
-    btns.forEach(button =>{ 
-        button.addEventListener('click',function game(e) {
-            playerSelection = getPlayerChoice(e);
-            computerSelection = getComputerChoice();
-            outcome = playRound(playerSelection,computerSelection);
-            switch (outcome) {
-                case "Tie":
-                    playerWins++;
-                    computerWins++;
-                    playerScore.textContent = `Player Score : ${playerWins}`;
-                    computerScore.textContent = `Computer Score : ${computerWins}`;
-                    break;
-                case "Victory":
-                    playerWins++;
-                    playerScore.textContent = `Player Score : ${playerWins}`;
-                    computerScore.textContent = `Computer Score : ${computerWins}`;
-                    break;
-                case "Defeat":
-                    computerWins++;
-                    playerScore.textContent = `Player Score : ${playerWins}`;
-                    computerScore.textContent = `Computer Score : ${computerWins}`;
-                    break;
-            }
-            i++
+function nextRound() {
+    i++;
+}
 
-            if (i == 6){
-                if (playerWins < computerWins){
-                    result.textContent = "You lose!";
-                    button.removeEventListener("click",game);
-                }else if (playerWins > computerWins){
-                    result.textContent = "You win!";
-                    button.removeEventListener("click",game);
-                }else {
-                    result.textContent = "Tie!";
-                    button.removeEventListener("click",game);
-                }
-            }
+function restartGame() {
+    location.reload();
+}
 
-        })})
-    
-    
-}  
+function waitForRestart(){
+    const confirmation = document.createElement("button");
+    confirmation.setAttribute("id","confirmation");
+    confirmation.classList.add("grow");
+    confirmation.textContent = "Click to restart";
+    resultContainer.appendChild(confirmation);
+    confirmation.addEventListener("click",restartGame);
+}
 
-initializeGame();
+function announceResult(){
+if (playerWins < computerWins){
+    result.textContent = "You lose!";
+}else if (playerWins > computerWins){
+    result.textContent = "You win!";
+}else {
+    result.textContent = "Tie!";
+    }
+}
 
+function playGame(e) {
+    if (i == 0) {initializeGame()};
+    if (i > 1) {hideComputerChoice()};
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice(e);
+    showComputerChoice(computerSelection);
+    outcome = playRound(playerSelection,computerSelection);
+    addPoints(outcome);
+    if (i == 5) {
+        announceResult();
+        buttons.forEach(button => button.removeEventListener("click",playGame));
+        waitForRestart();
+    };
+    nextRound();
+}
+
+function showComputerChoice(computerSelection) {
+    choiceImage = document.createElement("img");
+    choiceImage.setAttribute("id","choiceImage");
+    if (computerSelection == "rock"){
+        choiceImage.setAttribute("src","images/rock.webp");
+        choiceContainer.appendChild(choiceImage);
+    }else if (computerSelection == "paper"){
+        choiceImage.setAttribute("src","images/old-scroll-paper_108964-329.webp");
+        choiceContainer.appendChild(choiceImage);
+    }else {
+        choiceImage.setAttribute("src","images/istockphoto-1329468293-170667a.jpg");
+        choiceContainer.appendChild(choiceImage);
+    }
+}
+
+function hideComputerChoice(){
+    choiceImage = document.querySelector("#choiceImage");
+    choiceContainer.removeChild(choiceImage);
+}
